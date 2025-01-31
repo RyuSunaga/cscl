e71f8a1e66e8e2e0395309a59612cfc65814e49e
RyuSunaga <tetunoshin.ryu@icloud.com>
2025-01-31 09:03:04 +0900

    Add functions about initing and setting.

--- src/20-questions.lisp
+++ src/20-questions.lisp
@@ -1,6 +1,7 @@
 ;;; ゲーム20の質問を実装する
 
 (defparameter *answer* nil "answer")
+(defparameter *item-table* (make-hash-table) "hash table for item and infomation")
 
 (defparameter *items*
   `((elephant . ((animal-p . yes)
@@ -13,7 +14,7 @@
 	      (bigger-than-you . no)
 	      (fly-p . no)
 	      (eat-meat-p . no))))
-  "Items and their infomation of each category.")
+  "Items and their infomation. These are data for *item-table*.")
 
 (defparameter *categories*
   '((animal-p . (yes no))
@@ -21,21 +22,58 @@
     (bigger-than-you . (yes no))
     (fly-p . (yes no))
     (eat-meat-p . (yes no)))
-  "Categories and infomation"
-  )
+  "Categories and infomation")
 
-;; itemsの情報取得用のヘルパー関数
-(defun item-and-infomation (name)
-  "Return item and infomation"
-  (assoc name *items*))
+(defun init-item-table ()
+  "Init table for 20 questions."
 
-(defun item-name (name)
+  ;; Init table.
+  (setf *item-table* (make-hash-table))
+
+  ;; Check item-and-info-list from *items* and insert into *item-table*.
+  (mapc (lambda (x) (insert-item-and-info-list (item-name x) (info-list x))) *items*))
+
+(defun insert-item-and-info-list (name info-list)
+  "Insert a new item and infomation to *item-table* ."
+  (set-item name)
+  (mapc (lambda (x) (set-item-info name (info-category x) (info-value x))) info-list))
+
+;; Helper for *item-table*
+(defun set-item (name)
+  (setf (gethash name *item-table*) (make-hash-table)))
+
+(defun gethash-item-info (name)
+  (gethash name *item-table*))
+
+(defun set-item-info (item-name key value)
+  (setf (gethash key (gethash-item-info item-name)) value))
+
+(defun show-item-table ()
+  "Show item table for debugging."
+  (format t "===== Item Table =====~%")
+  (maphash (lambda (item item-info-table)
+	     (format t "item: ~a~%" item)
+	     (maphash (lambda (info value) (format t "info: ~a value: ~a~%" info value))
+		      item-info-table)
+	     (format t "-------------------~%"))
+	   *item-table*))
+
+;; Helper for *items*
+(defun info-category (info)
+  "Return a category from infomation."
+  (first info))
+
+(defun info-value (info)
+  "Return a category from infomation."
+  (rest info))
+
+(defun item-name (item-and-info-list)
   "Return item name"
-  (first (item-and-infomation name)))
+  (first item-and-info-list))
 
-(defun item-info (name)
+(defun info-list (item-and-info-list)
   "Return item infomation list"
-  (rest (item-and-infomation name)))
+  (rest item-and-info-list))
 
 
 ;; 回答候補を絞っていく
@@ -43,17 +81,18 @@
 
 (defun question ()
   "Question for user."
-  ;;
-  ;;
-  ;;
-
-  
+  ;; categoryの先頭から確認する
+  ;; categoryに紐づく要素を確認
+  ;; itemsの対応するカテゴリーの要素を確認して一致していれば削除
+  ;; itemの各要素を確認して要素数が0になっていれば全て一致するということなのでOK
   )
 
 
-
-
 (defun main ()
+
+  ;; Init *item-table* and set infomation from *items*.
+  (init-item-table)
+  (show-item-table)
 
   ;; Player decide answer
   (format t "Hi! Please input the answer in this game. ~%>>")
