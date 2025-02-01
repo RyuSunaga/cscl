66adf4adc625100a8cc7f6658f1d4f3b8a21b5cd
RyuSunaga <tetunoshin.ryu@icloud.com>
2025-02-01 12:08:15 +0900

    回答を元に情報を更新するプログラムを作成

--- src/20-questions.lisp
+++ src/20-questions.lisp
@@ -1,7 +1,7 @@
 ;;; ゲーム20の質問を実装する
-
 (defparameter *answer* nil "answer")
 (defparameter *item-table* (make-hash-table) "hash table for item and infomation")
+(defparameter *categories* nil)
 
 (defparameter *items*
   `((elephant . ((animal-p . yes)
@@ -16,7 +16,7 @@
 	      (eat-meat-p . no))))
   "Items and their infomation. These are data for *item-table*.")
 
-(defparameter *categories*
+(defparameter *simple-categories*
   '((animal-p . (yes no))
     (color . (red gray yellow))
     (bigger-than-you . (yes no))
@@ -26,10 +26,10 @@
 
 (defun init-item-table ()
   "Init table for 20 questions."
-
+  ;; Init Category
+  (setf *categories* *simple-categories*)  
   ;; Init table.
   (setf *item-table* (make-hash-table))
-
   ;; Check item-and-info-list from *items* and insert into *item-table*.
   (mapc (lambda (x) (insert-item-and-info-list (item-name x) (info-list x))) *items*))
 
@@ -44,6 +44,9 @@
 
 (defun gethash-item-info (item)
   (gethash item *item-table*))
+
+(defun item-info-value (item-name info-key)
+  (gethash info-key (gethash-item-info item-name)))
 
 (defun set-item-info (item-name key value)
   (setf (gethash key (gethash-item-info item-name)) value))
@@ -82,40 +85,55 @@
 (defun count-item-info (item)
   (hash-table-count (gethash item *item-table*)))
 
-(defun answer-p (item)
-  "Check the item is answer or not."
-  (if (= (count-item-info item) 0) t nil))
+;; Helpler for *categories*
+(defun get-category (category-name)
+  (assoc category-name *categories*))
 
-;; 回答候補を絞っていく
-;; 方針：*items*のitemに紐づくinfomationを一つずつ消していく。全て無くなればそれが回答と言える
+(defun category-name (category)
+  (first category))
+
+(defun category-values (category)
+  (rest category))
+
+(defun delete-category-value (category-name value)
+  "Delete value from category-name."
+  (delete value (assoc category-name *categories*)))
+
 
 (defun question ()
-  "Question for user."
+  "Question for user and return multiple values within category-name and value."
   ;; categoryの先頭から確認する
-  ;; categoryに紐づく要素を確認
-  ;; itemsの対応するカテゴリーの要素を確認して一致していれば削除
-  ;; itemの各要素を確認して要素数が0になっていれば全て一致するということなのでOK
-  )
+  (let* ((candidate-category (one-of-candidate-category))
+	 (category-name (category-name candidate-category))
+	 ;; 回答用のvalueを一つ選択
+	 (category-value (car (category-values candidate-category))))
+
+    (format t "~a is ~a ?~%" category-name category-value)
+    (values category-name category-value)))
+
+;; (defun test-multiple-values ()
+;;   (multiple-value-bind (category-name value) (question)
+;;     (format t "Multiple Value: ~a ~a ~%" category-name value)))
+
+(defun one-of-candidate-category ()
+  "Return one of category for answer."
+  ;; カテゴリーに紐づく属性の数が存在していれば回答候補のカテゴリー
+  (find-if #'(lambda (x) (category-values x)) *categories*))
+
 
 ;; helper for game.
+(defun init-game-settings ()
+  "Init game settings"
+  ;; init *answer*
+  (setf *answer* nil)
+  ;; Init *item-table* and set infomation from *items*.
+  (init-item-table))
 
 (defun read-line-as-symbol ()
   (intern (string-upcase (read-line))))
 
-(defun true-answer-p (answer)
-  "Check the answer is correct or not."
-  (if (eq answer *answer*) t nil))
-
-(defun main ()
-
-  ;; init *answer*
-  (setf *answer* nil)
-  
-  ;; Init *item-table* and set infomation from *items*.
-  (init-item-table)
-  (show-item-table)
-
-  ;; Player decide answer
+(defun read-answer-from-player ()
+  "Player decide answer in this game with prompt."
   (loop
     until *answer*
     do
@@ -123,22 +141,59 @@
        (setf *answer* (read-line-as-symbol))
        (if (gethash-item-info *answer*)
 	   (format t "Answer is ~a in this game.~%" *answer*)
-	   (setf *answer* nil)))
+	   (setf *answer* nil))))
+
+(defun read-yes-or-not-from-player ()
+  (format t "yes or no >> ")
+  (let ((answer (read-line-as-symbol)))
+    (if (member answer '(yes no))
+	answer
+	(read-yes-or-not-from-player))))
+
+(defun answer-p (item)
+  "Check the item is answer or not."
+  (if (= (count-item-info item) 0) t nil))
+
+(defun true-answer-p (answer)
+  "Check the answer is correct or not."
+  (if (eq answer *answer*) t nil))
+
+(defun main ()
+  "Main for 20 Questions."
+
+  (init-game-settings)
+
+  ;; show table for user.
+  (show-item-table)
+  
+  ;; set answer in this game.
+  (read-answer-from-player)
   
   ;; 20 questions
   (dotimes (i 20)
-    ;; Programm ask question or answer.
 
-    ;; Player answer for the question.
-
-    ;; TODO: questoinの回答で分岐
-    ;; 質問した属性がyesなら：全リストでその要素を持つものを削除する
-    ;; 質問した属性がnoなら：カテゴリーの情報を削除
-    ;; カテゴリーの情報が1つになってしまったら：全リストでその情報を持つものを削除する
+    ;; TODO: 解答が確定しているなら返す
+    ;; Question about answer.
     
-    )
+    ;; Question about category.
+    (multiple-value-bind (category-name value) (question)
+      
+      ;; Player answer for the question.
+      (when (eq (read-yes-or-not-from-player) 'yes)
+	
+	;; *item-table*から該当の要素を全て消したい (e.q. 'animal-p 'yes の要素を持つinfoを全て消す
+	(maphash (lambda (item _)
+		   (when (eq (item-info-value item category-name) value)
+		     (format t "DELETE ~a ~a ~a%" item category-name value)
+		     ;; *item-tableから該当の要素を消す*
+		     (delete-item-info item category-name)))
+		 *item-table*))
+      
+      ;; 今回の質問をできないようにカテゴリーを更新する
+      (delete-category-value category-name value)
 
-  ;; Show the answer. (Programm lose.)
+      ;; debuggin
+      (show-item-table)))
 
-  )
-
+  ;; Show the answer.
+  (format t "Player is WIN!!~% Answer is ~a~%" *answer*))
